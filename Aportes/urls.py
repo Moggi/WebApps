3837d7ed9dd83ff207c55ee81e0a08eeca49f27c
Paulo Moggi <paulohpmoggi@gmail.com>
2017-11-21 10:25:10 -0200

    Refactor the Aportes/mensal

--- Aportes/urls.py
+++ Aportes/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^mensal_fixo', views.mensal_fixo, name='mensal_fixo'),
-    url(r'^mensal_progressivo', views.mensal_progressivo, name='mensal_progressivo'),
+    url(r'^mensal', views.mensal, name='mensal'),
 ]
--- Aportes/views.py
+++ Aportes/views.py
@@ -19,11 +19,12 @@ def index(request):
 # POST Only methods
 # ==============================================================================
 @csrf_exempt
-def mensal_progressivo(request):
+def mensal(request):
     if(request.method == 'POST'):
         capital_inicial = float(request.POST['capital_inicial'])
         aportes_mensais = float(request.POST['aportes_mensais'])
         taxa_mensal     = float(request.POST['taxa_mensal'])
+        taxa_progressiva = float(request.POST['taxa_progressiva'])
 
         taxa_mensal     = (taxa_mensal/100) if taxa_mensal >= 0 else 1
 
@@ -57,54 +58,8 @@ def mensal_progressivo(request):
             if (valor_anual-capital_investido)*(0.775)/1000000 >= 1:
                 break
 
-            aportes_mensais += aportes_mensais*0.05
-
-        context = getContext({
-            'capital_inicial': capital_inicial,
-            'taxa_mensal': taxa_mensal,
-            'valores_mensais': valores_mensais,
-        })
-        return render(request, 'Aportes/mensal.html', context)
-    return redirect('/aportes')
-
-@csrf_exempt
-def mensal_fixo(request):
-    if(request.method == 'POST'):
-        capital_inicial = float(request.POST['capital_inicial'])
-        aportes_mensais = float(request.POST['aportes_mensais'])
-        taxa_mensal     = float(request.POST['taxa_mensal'])
-
-        taxa_mensal     = (taxa_mensal/100) if taxa_mensal >= 0 else 1
-
-        capital_investido = capital_inicial
-
-        valor_anual = capital_inicial
-
-        valores_mensais = []
-
-        it = 100 # 100 anos
-        for it in range(1, 100 +1):
-
-            valor_mensal = valor_anual
-            for mes in range(1, 12 +1):
-                capital_investido += aportes_mensais
-                valor_mensal = (valor_mensal+aportes_mensais)*(1+taxa_mensal)
-
-            valor_anual = valor_mensal
-
-            valores_mensais.append({
-                'aportes_mensais': aportes_mensais,
-                'capital_investido': capital_investido,
-                'valor_anual': valor_anual,
-                'lucro_anual': valor_anual-capital_investido,
-                'lucro_anual_15': (valor_anual-capital_investido)*0.85,
-                'lucro_anual_175': (valor_anual-capital_investido)*0.825,
-                'lucro_anual_20': (valor_anual-capital_investido)*0.80,
-                'lucro_anual_225': (valor_anual-capital_investido)*0.775,
-            })
-
-            if (valor_anual-capital_investido)*(0.775)/1000000 >= 1:
-                break
+            if taxa_progressiva > 0.0001:
+                aportes_mensais += aportes_mensais*taxa_progressiva
 
         context = getContext({
             'capital_inicial': capital_inicial,

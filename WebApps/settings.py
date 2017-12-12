50f70f20767d6828de7bec01ce9797f69c6003ba
Paulo Moggi <paulohpmoggi@gmail.com>
2017-12-12 11:08:39 -0200

    Automatic find static files

--- WebApps/settings.py
+++ WebApps/settings.py
@@ -142,8 +142,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/s/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_FINDER = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]

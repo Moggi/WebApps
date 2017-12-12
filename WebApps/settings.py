65689bc183ca532ab55a863aad5fb72fd2caa149
Paulo Moggi <paulohpmoggi@gmail.com>
2017-12-12 09:56:02 -0200

    Update StaticFiles Storage

--- WebApps/settings.py
+++ WebApps/settings.py
@@ -142,9 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/s/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "../static"),
 ]

ab6b7477f39f4301b194790e7e1a3e8db966b0fd
Paulo Moggi <paulohpmoggi@gmail.com>
2017-12-20 09:58:22 -0200

    Update Apps Urls

--- Aportes/urls.py
+++ Aportes/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='Aportes'),
     url(r'^mensal', views.mensal, name='mensal'),
 ]
--- Todo/urls.py
+++ Todo/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='Todo'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details, name='details'),
     url(r'^add', views.add, name='add'),
     url(r'^delete', views.delete, name='delete'),
